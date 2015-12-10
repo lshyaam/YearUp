d0a82798f32537bfced716c2ca260560f1593569
lshyaam <Lshyaam@atl.yearup.org>
2015-12-10 11:34:04 -0500

    af

---
+++ InternshipAssessment/src/LCRoster.java
@@ -0,0 +1,42 @@
+public class LCRoster
+{
+		String m_studentname;
+		int m_lcID;
+		String m_lcName;
+
+		public static <m_lcName> void main(String[] args)
+		{
+
+	
+	LCRoster
+	m_studentname = null;
+	m_lcID = null;
+	m_lcName = null;
+	
+
+}
+
+		public String getM_studentname() {
+			return m_studentname;
+		}
+
+		public void setM_studentname(String m_studentname) {
+			this.m_studentname = m_studentname;
+		}
+
+		public int getM_lcID() {
+			return m_lcID;
+		}
+
+		public void setM_lcID(int m_lcID) {
+			this.m_lcID = m_lcID;
+		}
+
+		public String getM_lcName() {
+			return m_lcName;
+		}
+
+		public void setM_lcName(String m_lcName) {
+			this.m_lcName = m_lcName;
+		}
+		}
